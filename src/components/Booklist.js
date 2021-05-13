143aaaf6ec1e9400f3c7c3ea12c26b9e3e1a5b85
AJ <aj.monty@gmail.com>
2021-05-13 11:06:20 +0100

    first commit with initial set up

---
+++ src/components/Booklist.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Booklist extends Component {
+    render() { 
+        return ( 
+            <div className='book-list'>
+                <ul>
+                    <li>the design of everyday things</li>
+                    <li>don't make me think</li>
+                    <li>operation trekker</li>
+                </ul>
+            </div>
+         );
+    }
+}
+ 
+export default Booklist;
